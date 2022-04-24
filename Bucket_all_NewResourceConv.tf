8162d2e74c7f6c3aac7dfd62d474a7b06223c890
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-04-24 14:57:30 +0300

    Add files via upload

---
+++ Bucket_all_NewResourceConv.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my_tf_test_bucket"
+}
+
+resource "aws_s3_bucket_policy" "test_bucket_policy" {
+  bucket = aws_s3_bucket.test_bucket.id
+  policy =<<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "MYBUCKETPOLICY",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:*",
+      "Resource": "aws_s3_bucket.my_tf_test_bucket.arn/*"
+          }
+  ]
+}
+POLICY
+}
