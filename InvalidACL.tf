36536bcc5e87c0abf7821464e90e7c9c096d7358
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-04-26 11:36:56 +0300

    Add files via upload

---
+++ InvalidACL.tf
@@ -0,0 +1,21 @@
+{
+  "owner": {
+    id = 9c8b1505c84b4a4c79ca103b7561c42fdce5953c95d387cbd5f8dbf9e12074e1
+  }
+  "grant 
+    {
+      grantee {
+        id = 9c8b1505c84b4a4c79ca103b7561c42fdce5953c95d387cbd5f8dbf9e12074e1
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+    grant
+    {
+      grantee {
+        type = "Group",
+        uri = "http://acs.amazonaws.com/groups/global/AllUsers"
+      }
+      permission = "READ"
+    }
+ }
